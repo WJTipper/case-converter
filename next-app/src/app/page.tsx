5d599fdee0dfad75783447ccfc7817729c5f8f98
WJTipper <will.tipper@hotmail.co.uk>
2024-10-19 19:59:13 +0100

    Refactor form, remove components folder

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,23 +1,53 @@
-import Button from "@/components/button/button";
-import Dropdown from "@/components/dropdown/dropdown";
-import Input from "@/components/input/input";
+"use client";
 
 export default function Home() {
+	const caseList: string[] = ["mumble", "scream"];
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		const data = new FormData(event.currentTarget);
+		const inputStr = data.get("inputStr");
+		const inputCase = data.get("inputCase");
+		const outputCase = data.get("outputCase");
+	};
+
 	return (
-		<form>
-			<Input id={"input-string"} labelText={"Input string:"} />
-			<br />
-			<Dropdown
-				selectedOption="Input case"
-				optionList={["option1", "option2"]}
-			/>
-			<br />
-			<Dropdown
-				selectedOption="Output case"
-				optionList={["option1", "option2"]}
-			/>
-			<br />
-			<Button text={"Submit"} />
-		</form>
+		<div>
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="input-str">Input string</label>
+				<br />
+				<input id="input-str" name="inputStr" type="text" />
+				<br />
+				<label htmlFor="input-str">Input case</label>
+				<br />
+				<select
+					id="input-case"
+					name="inputCase"
+					className="form-select"
+				>
+					<option selected>{}</option>
+					{caseList.map((item, index) => {
+						return <option key={index}>{item}</option>;
+					})}
+				</select>
+				<br />
+				<label htmlFor="output-str">Output case</label>
+				<br />
+				<select
+					id="output-case"
+					name="outputCase"
+					className="form-select"
+				>
+					<option selected>{}</option>
+					{caseList.map((item, index) => {
+						return <option key={index}>{item}</option>;
+					})}
+				</select>
+				<br />
+				<button type="submit" className="btn btn-primary">
+					Submit
+				</button>
+			</form>
+		</div>
 	);
 }
