c819a6d2e397d4d106a25707308f5e644624eb5b
WJTipper <will.tipper@hotmail.co.uk>
2024-10-27 14:06:23 +0000

    Move functions to src/functions/ folder

--- next-app/src/functions/validate-inputs.test.ts
+++ next-app/src/functions/validate-inputs.test.ts
@@ -1,27 +1,4 @@
-import {convertString, validateInputs} from "./functions"
-
-const convertStringCases = [
-    ["thisIsALongString", "camel", "kebab", "this-is-a-long-string"],
-    ["thisIsALongString", "camel", "pascal", "ThisIsALongString"],
-    ["thisIsALongString", "camel", "snake", "this_is_a_long_string"],
-    ["this-is-a-long-string", "kebab", "pascal", "ThisIsALongString"],
-    ["this-is-a-long-string", "kebab", "snake", "this_is_a_long_string"],
-    ["this-is-a-long-string", "kebab", "scream", "THIS_IS_A_LONG_STRING"],
-    ["ThisIsALongString", "pascal", "snake", "this_is_a_long_string"],
-    ["ThisIsALongString", "pascal", "scream", "THIS_IS_A_LONG_STRING"],
-    ["ThisIsALongString", "pascal", "train", "THIS-IS-A-LONG-STRING"],
-    ["this_is_a_long_string", "snake", "scream", "THIS_IS_A_LONG_STRING"],
-    ["this_is_a_long_string", "snake", "train", "THIS-IS-A-LONG-STRING"],
-    ["this_is_a_long_string", "snake", "mumble", "thisisalongstring"],
-    ["THIS_IS_A_LONG_STRING", "scream", "train", "THIS-IS-A-LONG-STRING"],
-    ["THIS_IS_A_LONG_STRING", "scream", "mumble", "thisisalongstring"],
-    ["THIS_IS_A_LONG_STRING", "scream", "camel", "thisIsALongString"],
-    ["THIS-IS-A-LONG-STRING", "train", "mumble", "thisisalongstring"],
-    ["THIS-IS-A-LONG-STRING", "train", "camel", "thisIsALongString"],
-    ["THIS-IS-A-LONG-STRING", "train", "kebab", "this-is-a-long-string"],
-    ["string", "invalidCase", "snake", ""],
-    ["string", "snake", "invalidCase", ""],
-]
+import { validateInputs } from "./validate-inputs"
 
 const validateInputsCases = [
     ["", "", "", "Empty input string: Please enter an input string"],
@@ -46,15 +23,6 @@ const validateInputsCases = [
     ["THIS_IS_A_LONG_STRING", "scream", "train", ""],
     ["THIS-IS-A-LONG-STRING", "train", "mumble", ""]
 ]
-
-describe("convertString function", () => {
-    test.each(convertStringCases) ("given %p, %p, %p as arguments, returns %p",
-        (inputStr, inputCase, outputCase, expectedResult) => {
-            const outputStr = convertString(inputStr, inputCase, outputCase)
-            expect(outputStr).toEqual(expectedResult)
-        }
-    )
-})
 
 describe("validateInputs function", () => {
     test.each(validateInputsCases) ("given %p, %p, %p as arguments, returns %p",
